269e844490f5f2bc05e3348f29d579c6852ac77a
mortendk <morten@thememachine.io>
2022-07-17 23:47:19 +0200

    Update service-workers.js

--- src/service-workers.js
+++ src/service-workers.js
@@ -1,7 +1,7 @@
 // This code executes in its own worker or thread
 self.addEventListener("install", (event) => {
-  console.log("Service worker installed");
+  // console.log("Service worker installed");
 });
 self.addEventListener("activate", (event) => {
-  console.log("Service worker activated");
+  // console.log("Service worker activated");
 });
