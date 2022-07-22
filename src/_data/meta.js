664aa041a4a5dd5d714584f4812da7bb03d85c1d
mortendk <morten@thememachine.io>
2022-07-23 01:01:57 +0200

    Update meta.js

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -2,6 +2,6 @@
 module.exports = {
   env: process.env.ELEVENTY_ENV,
   // NOTE: `process.env.URL` is provided by Netlify, and may need
+  // url: process.env.URL || "http://localhost:8080",
   // adjusted pending your host
-  url: process.env.URL || "http://localhost:8080",
 };
