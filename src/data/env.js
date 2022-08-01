3abb8ead0dee95bc4ca57c04ecd7fa18ed6c9ce9
mortendk <morten@thememachine.io>
2022-08-01 18:39:26 +0200

    site url ok

--- src/data/env.js
+++ src/data/env.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 module.exports = {
-  url: process.env.URL || "http://localhost:8080",
+  url: process.env.SITE_URL || "http://localhost:8080",
   mode: process.env.ELEVENTY_ENV || "prod",
   root: process.env.ELEVENTY_ROOT,
   source: process.env.ELEVENTY_SOURCE,
