8b087960d8280923693621172214c901a60a40f4
mortendk <morten@thememachine.io>
2022-08-01 18:33:53 +0200

    move env

--- src/data/env.js
+++ src/data/env.js
@@ -3,4 +3,7 @@ require("dotenv").config();
 module.exports = {
   url: process.env.URL || "http://localhost:8080",
   mode: process.env.ELEVENTY_ENV || "prod",
+  root: process.env.ELEVENTY_ROOT,
+  source: process.env.ELEVENTY_SOURCE,
+  serverless: process.env.ELEVENTY_SERVERLESS || "nope",
 };
