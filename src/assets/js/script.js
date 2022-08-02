7a5b4cd93d6c79f99c32fdea39ba9e135ddc710b
mortendk <morten@thememachine.io>
2022-08-02 23:34:58 +0200

    testing alpine

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -13,6 +13,7 @@ btn.onclick = function () {
 let btnImageToggle = document.querySelector(".js-image-toggle");
 btnImageToggle.onclick = function (e) {
   e.preventDefault();
+  document.body.classList.toggle("overflow-y-hidden");
   this.nextElementSibling.classList.toggle("gallery-open");
   this.nextElementSibling.classList.toggle("z-0");
 };
