a959e4b1c801cd56072ec65af3baed8429bee4dd
mortendk <morten@thememachine.io>
2022-08-02 22:57:07 +0200

    toggler

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -3,10 +3,16 @@ let btn = document.querySelector(".js-menu-toggle");
 let menu = document.querySelector(".js-menu");
 
 btn.onclick = function () {
-  // menu.classList.toggle("hidden");
   menu.classList.toggle("-left-full");
   menu.classList.toggle("left-0");
-  // show hide the menu icons
   btn.children[0].classList.toggle("hidden");
   btn.children[1].classList.toggle("hidden");
+};
+
+// image toggler
+let btnImageToggle = document.querySelector(".js-image-toggle");
+btnImageToggle.onclick = function (e) {
+  e.preventDefault();
+  this.nextElementSibling.classList.toggle("gallery-open");
+  this.nextElementSibling.classList.toggle("z-0");
 };
