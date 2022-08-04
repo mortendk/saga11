a9b2f5c892ff83c29322a7fbc750198d22132c1c
mortendk <morten@thememachine.io>
2022-08-04 15:13:54 +0200

    adding the defaults

--- src/assets/js/script.js
+++
@@ -1,8 +0,0 @@
-// image toggler
-// let btnImageToggle = document.querySelector(".js-image-toggle");
-// btnImageToggle.onclick = function (e) {
-//   e.preventDefault();
-//   document.body.classList.toggle("overflow-y-hidden");
-//   this.nextElementSibling.classList.toggle("gallery-open");
-//   this.nextElementSibling.classList.toggle("z-0");
-// };
