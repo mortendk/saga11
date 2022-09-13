ac337d3b455e2d804bb45abcfc061313e610f9cb
mortendk <morten@thememachine.io>
2022-09-13 17:16:13 +0200

    Update env.js

--- src/data/env.js
+++ src/data/env.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
 
 module.exports = {
-  url: process.env.SITE_URL || process.env.URL,
   mode: process.env.ELEVENTY_ENV || "prod",
+  url: process.env.SITE_URL || process.env.URL,
+  branch: process.env.BRANCH || "main",
   root: process.env.ELEVENTY_ROOT,
   source: process.env.ELEVENTY_SOURCE,
   serverless: process.env.ELEVENTY_SERVERLESS || "",
