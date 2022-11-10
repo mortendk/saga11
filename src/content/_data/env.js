c28eeeed1534df561ab1d355f7602ad73cb05979
mortendk <morten@thememachine.io>
2022-11-10 09:38:23 +0100

    settings for the local admin port

--- src/content/_data/env.js
+++ src/content/_data/env.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 module.exports = {
   mode: process.env.ELEVENTY_ENV || "prod",
   url: process.env.SITE_URL || process.env.URL,
+  netlifycmsport: process.env.PORT || "8082",
   branch: process.env.BRANCH || "main",
   root: process.env.ELEVENTY_ROOT,
   source: process.env.ELEVENTY_SOURCE,
