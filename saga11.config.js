dba1fcd186ec7576b433deb8883f290a3aae62ac
mortendk <morten@thememachine.io>
2023-06-26 16:40:04 +0200

    cleaning up theme settings now also in package

--- saga11.config.js
+++ saga11.config.js
@@ -1,9 +1,9 @@
 // Settings for saga11
-// theme sets the theme path : nakinn / grunn
 // dateLocal for formating dates
+
 module.exports = {
   theme: "theme-grunn",
   dateLocalize: "Da",
   timeZone: "Europe/Copenhagen"
 };
-// TODO: datelocal settings
+
