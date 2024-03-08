571c116fc2d8b9bf8d79b3c04fbf18b4305e4b7f
mortendk <hey@mortendk.dev>
2024-03-08 15:01:50 +0100

    testing minifier

--- eleventy.config.js
+++ eleventy.config.js
@@ -26,6 +26,8 @@ const markdownItAnchor = require('markdown-it-anchor');
 const embedYouTube = require("eleventy-plugin-youtube-embed");
 const embedVimeo = require("eleventy-plugin-vimeo-embed");
 
+const eleventyPluginFilesMinifier = require("@sherby/eleventy-plugin-files-minifier");
+
 module.exports = function (eleventyConfig) {
   // Plugins
   eleventyConfig.addPlugin(pluginRss);
@@ -43,7 +45,7 @@ module.exports = function (eleventyConfig) {
         // the wrapper to use around the generated TOC
         return `${toc}`;
     },
-});
+  });
 
   // Shortcodes
   eleventyConfig.addShortcode("image", require("./src/_system/11ty/shortcode/image.js"));
@@ -109,20 +111,20 @@ module.exports = function (eleventyConfig) {
     typographer: true
   }).use(
     markdownItEleventyImg, {
-    imgOptions: {
-      widths: [640, 1200, 1500],
-      urlPath: "/img",
-      outputDir: "./_site/img/",
-      formats: ["webp"]
-    },
-    globalAttributes: {
-      class: "md-image",
-      decoding: "async",
-      loading: "lazy",
-      sizes: "(min-width: 30em) 50vw, 100vw"
-    },
-    resolvePath: (filepath) => path.join('src', filepath)
-  }
+      imgOptions: {
+        widths: [640, 1200, 1500],
+        urlPath: "/img",
+        outputDir: "./_site/img/",
+        formats: ["webp"]
+      },
+      globalAttributes: {
+        class: "md-image",
+        decoding: "async",
+        loading: "lazy",
+        sizes: "(min-width: 30em) 50vw, 100vw"
+      },
+      resolvePath: (filepath) => path.join('src', filepath)
+    }
   ).use(markdownItAnchor, {})
 
   );
@@ -139,6 +141,13 @@ module.exports = function (eleventyConfig) {
     embedClass: 'md-video',
     dnt: true
   });
+
+  // HTML minify
+  // if (env.mode == "prod") {
+    // eleventyConfig.addTransform("htmlmin", require("./src/_system/11ty/transform/minify.js"));
+    eleventyConfig.addPlugin(eleventyPluginFilesMinifier);
+
+  // }
 
   return {
     dir: {
