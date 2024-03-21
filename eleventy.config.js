bc82fa7f38ec3cff2296c6bfcd5fab8a379cd3c0
mortendk <hey@mortendk.dev>
2024-03-21 13:49:37 +0100

    Update eleventy.config.js

--- eleventy.config.js
+++ eleventy.config.js
@@ -159,7 +159,6 @@ module.exports = function (eleventyConfig) {
   if (env.mode == "prod") {
     eleventyConfig.addPlugin(eleventyPluginFilesMinifier);
     eleventyConfig.addPlugin(criticalCss, {
-        base: "_site",
         dimensions : [
           { width: 414, height: 896 },
           { width: 1920, height: 1080 }
