132c2e84937a0161a9eb59b2acc24adaef948250
mortendk <hey@mortendk.dev>
2024-04-08 12:16:45 +0200

    renaming the youtube warning

---
+++ src/theme-grunn/assets/js/saga11.js
@@ -0,0 +1,13 @@
+// General utilities used by 11ty
+// 👋 src/theme-grunn/assets/js/saga11.js
+//
+
+// Set a warning under rendered youtube videos that if they press play etc
+const tube = document.querySelectorAll('.md-youtube')
+const div = document.createElement("div");
+div.textContent = "FYI: When clicking play on the youtube video - youtube will set cookies";
+// div.classList.add('')
+
+tube.forEach(function (element) {
+  element.appendChild(div);
+});
