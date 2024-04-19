4689962fb45ad38659ae63ccfdf21afbbae1c1b1
mortendk <hey@mortendk.dev>
2024-04-19 23:33:00 +0200

    adding decap medialib support

--- src/content/_data/env.js
+++ src/content/_data/env.js
@@ -4,10 +4,16 @@ module.exports = {
   mode: process.env.ELEVENTY_ENV || "prod",
   url: process.env.SITE_URL || "http://localhost:3791",
   siteport: process.env.SITE_PORT || "3791",
-  decapcmsport: process.env.PORT || "8082",
-  decapcmsworkflow: process.env.DECAP_WORKFLOW || "simple",
   branch: process.env.BRANCH || "main",
   root: process.env.ELEVENTY_ROOT,
   source: process.env.ELEVENTY_SOURCE,
   serverless: process.env.ELEVENTY_SERVERLESS || "",
+
+  decapcmsport: process.env.PORT || "8082",
+  decapcmsworkflow: process.env.DECAP_WORKFLOW || "simple",
+
+  decapcmsmedialibrary: process.env.DECAP_MEDIALIBRARY,
+  uploadcarekey: process.env.UPLOADCARE_KEY,
+  cloudinaryname: process.env.CLOUDINARY_NAME,
+  cloudinarykey: process.env.CLOUDINARY_KEY,
 };
